10cde724765303b9a3f279c628bfa536d7860c52
むずでょ <muzudho1@gmail.com>
2021-01-22 21:07:01 +0900

    entryConf

--- controller/client.go
+++ controller/client.go
@@ -14,22 +14,26 @@ type Client struct {
 	entryConf EntryConf
 }
 
+type clientListener struct {
+	// １行で 1024 byte は飛んでこないことをサーバーと決めておけだぜ☆（＾～＾）
+	lineBuffer [1024]byte
+	index      uint
+}
+
 // Spawn - クライアント接続
 func (client Client) Spawn(entryConf EntryConf) error {
 	client.entryConf = entryConf
-	return telnet.DialToAndCall(fmt.Sprintf("%s:%d", entryConf.Nngs.Host, entryConf.Nngs.Port), clientListener{})
+	return telnet.DialToAndCall(fmt.Sprintf("%s:%d", entryConf.Nngs.Host, entryConf.Nngs.Port), clientListener{index: 0})
 }
-
-type clientListener struct{}
 
 // CallTELNET - 決まった形のメソッド。
 func (c clientListener) CallTELNET(ctx telnet.Context, w telnet.Writer, r telnet.Reader) {
-	go read(r)
+	go c.read(r)
 	write(w)
 }
 
 // 送られてくるメッセージを待ち構えるループです。
-func read(r telnet.Reader) {
+func (c clientListener) read(r telnet.Reader) {
 	var buffer [1]byte // これが満たされるまで待つ。1バイト。
 	p := buffer[:]
 
@@ -38,7 +42,16 @@ func read(r telnet.Reader) {
 
 		if n > 0 {
 			bytes := p[:n]
+			c.lineBuffer[c.index] = bytes[0]
+			c.index++
+			// 改行がない行も届くので、改行が届くまで待つという処理ができません。
 			print(string(bytes)) // 受け取るたびに表示。
+
+			// 行末を判定できるか☆（＾～＾）？
+			if bytes[0] == '\n' {
+				// print("行末だぜ☆（＾～＾）！")
+				c.index = 0
+			}
 		}
 
 		if nil != err {
--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 
 func main() {
 	// fmt.Println("設定ファイルを読み込んだろ☆（＾～＾）")
-	ui.LoadEntryConf("./input/default.entryConf.toml")
+	entryConf := ui.LoadEntryConf("./input/default.entryConf.toml")
 
 	fmt.Println("(^q^) 何か文字を打てだぜ☆ 終わりたかったら [Ctrl]+[C]☆")
 	client := c.Client{}
-	client.Spawn()
+	client.Spawn(entryConf)
 	fmt.Println("(^q^) おわり☆！")
 }
